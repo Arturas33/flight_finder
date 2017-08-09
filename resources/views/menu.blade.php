aef74603ee76e940433ac7c911f35dd2902a9ee0
Arturas33 <jasinskis330@gmail.com>
2017-08-09 14:06:18 +0300

    added menu navbar

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,7 +1,20 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Arturas
- * Date: 8/8/2017
- * Time: 2:43 PM
- */
+<nav class="navbar  navbar-inverse">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="{{'/country'}}">Country</a></li>
+                <li><a href="{{'/airline'}}">Air line</a></li>
+                <li><a href="{{'/airports'}}">Air ports</a></li>
+                <li><a href="{{'/flights'}}">Flights</a></li>
+            </ul>
+        </div><!-- /.nav-collapse -->
+    </div><!-- /.container -->
+</nav>
