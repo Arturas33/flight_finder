7aa6e94af6a42d86f909fa2e8011b6bfe77fc256
Arturas33 <jasinskis330@gmail.com>
2017-08-11 15:41:16 +0300

    create function

--- app/Models/FFFlights.php
+++ app/Models/FFFlights.php
@@ -18,4 +18,21 @@ class FFFlights extends CoreModel
      */
 
     protected $fillable = ['id', 'name', 'departure', 'arrivel', 'airLine_id', 'origin_id', 'destination_id'];
+
+
+    protected $with = ['originAirport','destinationAirport','airline'];
+
+    public function originAirport()
+    {
+        return $this->hasOne(FFAirPorts::class, 'id', 'origin_id');
+    }
+
+    public function destinationAirport()
+    {
+        return $this->hasOne(FFAirPorts::class, 'id', 'destination_id');
+    }
+    public function airline()
+    {
+        return $this->hasOne(FFAirLine::class, 'id', 'airLine_id');
+    }
 }
